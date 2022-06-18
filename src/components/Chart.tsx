8a675af6134616f413d324ebe3a5c661f67f25a0
uriellevy <uriellevy4892@gmail.com>
2022-06-18 14:11:59 +0300

    date object created

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -5,25 +5,25 @@ import "./Chart.scss";
 
 
 export default class Chart extends Component {
-    state = {
-        hover: false,
-    }
+  
   render() {
-    const startDate = new Date("2019-01-07T00:32:00.649Z");
-    const endDate = new Date("2020-01-06T03:52:39.016Z");
-    const datesOfFuLLYear = [];
-    while(startDate <= endDate) {
-      datesOfFuLLYear.push(new Date(startDate).toISOString());
-      startDate.setDate(startDate.getDate() + 1)
-    }
-    const d = new Date("2019-01-07T00:32:00.649Z").toLocaleDateString();
-    console.log(d);
-    
+    const datesObject:{ [key: string]: number } = {};
+    const dateHandler = () => {
+      const startDate = new Date("2019-01-07T00:32:00.649Z");
+      const endDate = new Date("2020-01-06T03:52:39.016Z");
+      while(startDate <= endDate) {
+        datesObject[startDate.toLocaleDateString()] = 0;
+        startDate.setDate(startDate.getDate() + 1)
+       }
+
+  }
+  // console.log(datesObject)
+    dateHandler();
     return (
       <ul className='chart-container'>
-          {datesOfFuLLYear.map((date, idx) => (
+          {/* {datesOfFuLLYear.map((date, idx) => (
             <ChartItem date={date} key={idx}/>
-          ))}
+          ))} */}
       </ul>
     )
   }
