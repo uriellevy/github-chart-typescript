e83d9ac94991ecba959d4b76ffb456b8ce107fc7
uriellevy <uriellevy4892@gmail.com>
2022-06-19 17:05:15 +0300

    hex colors dynamic classNames added

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,32 +3,35 @@ import dates from '../data';
 import ChartItem from './ChartItem';
 import "./Chart.scss";
 
+const datesObject:{ [key: string]: number } = {};
 
 export default class Chart extends Component {
+  dateHandler = () => {
+   const startDate = new Date("2019-01-07T00:32:00.649Z");
+   const endDate = new Date("2020-01-06T03:52:39.016Z");
+   while(startDate <= endDate) {
+     datesObject[startDate.toLocaleDateString()] = 0;
+     startDate.setDate(startDate.getDate() + 1)
+    }
+   for (let date of dates) {
+     if (datesObject[new Date(date).toLocaleDateString()] === 0) {
+       datesObject[new Date(date).toLocaleDateString()] = 1
+     } else datesObject[new Date(date).toLocaleDateString()] += 1;
+   }
+}
+// componentDidMount() {
+  //   this.dateHandler();
+  // }
   
   render() {
-    const datesObject:{ [key: string]: number } = {};
-    const dateHandler = () => {
-      const startDate = new Date("2019-01-07T00:32:00.649Z");
-      const endDate = new Date("2020-01-06T03:52:39.016Z");
-      while(startDate <= endDate) {
-        datesObject[startDate.toLocaleDateString()] = 0;
-        startDate.setDate(startDate.getDate() + 1)
-       }
-     console.log(typeof new Date(dates[0]).toLocaleDateString())
-     for (let date of dates) {
-       if (datesObject[new Date(date).toLocaleDateString()] === 0) {
-        datesObject[new Date(date).toLocaleDateString()] = 1
-       } else datesObject[new Date(date).toLocaleDateString()] += 1;
-     }
-  }
-  dateHandler();
-  console.log(datesObject)
+    this.dateHandler();
+  
+  // console.log(Object.entries(datesObject))
     return (
       <ul className='chart-container'>
-          {/* {datesOfFuLLYear.map((date, idx) => (
-            <ChartItem date={date} key={idx}/>
-          ))} */}
+          {Object.entries(datesObject).map((dateItem, idx) => (
+            <ChartItem key={idx} dateItem={dateItem}/>
+          ))}
       </ul>
     )
   }
--- src/components/ChartItem.tsx
+++ src/components/ChartItem.tsx
@@ -3,16 +3,19 @@ import "./ChartItem.scss"
 import data from "../data";
 
 interface ChartItemProps {
-    date: string
+    dateItem: any[]
 }
+
+
 
 export default class ChartItem extends Component<ChartItemProps, {}> {
 
   render() {
-    //   console.log(data)
-      const {date} = this.props;
+    const {dateItem} = this.props;
+    const itemColor = dateItem[1] === 0 ? 'zero-contributions' : dateItem[1] > 0 && dateItem[1] < 4 ? 'light-color' : dateItem[1] > 3 && dateItem[1] < 8 ? 'medium-color' : dateItem[1] > 7 && 'heavy-color';
+    console.log(dateItem[1])
     return (
-      <li className='chart-item-wrapper'></li>
+      <li className={`chart-item-wrapper ${itemColor}`}></li>
     )
   }
 }
